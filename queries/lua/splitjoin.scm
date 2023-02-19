81def5c0a4b4a26ba36e44f1b881434214fbe5e2
Benny Powers <web@bennypowers.com>
2023-02-19 10:03:47 +0200

    feat(lua): variable_list
    closes #7

--- queries/lua/splitjoin.scm
+++ queries/lua/splitjoin.scm
@@ -2,4 +2,5 @@
   (table_constructor)
   (parameters)
   (arguments)
+  (variable_list)
 ] @_splitjoin
--- test/lua.lua
+++ test/lua.lua
@@ -5,6 +5,7 @@ local table = { a = 'a', b = 'b', c = 'c' }
 local mixed = { 1, 2, 3, a = 'a', b = 'b', c = 'c' }
 
 local function params(a, b, c)
+  a, b, c = mod(a, b, c)
 end
 
-params(1, 2, 3)
+a, b, c = d
