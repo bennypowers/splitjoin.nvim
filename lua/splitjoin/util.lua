b0b5c957a01b7484b9d1a238a2c7fd7ff0c902b9
Benny Powers <web@bennypowers.com>
2023-02-22 21:57:27 +0200

    refactor(lua): variable lists

--- lua/splitjoin/util.lua
+++ lua/splitjoin/util.lua
@@ -1,29 +1,15 @@
 local map = vim.tbl_map
+local get_node_at_pos = vim.treesitter.get_node_at_pos
 
 local M = {}
 
 local OPTIONS = {}
 local DEFAULTS = {}
 
-local function get_config_for(key, fallback)
-  return function(lang, type, op)
-    local mod = DEFAULTS.languages[lang]
-    -- TODO: make everything a node handler
-    if key == 'handlers' then
-      return (mod and mod[key] and mod[key][type] and mod[key][type][op]) or fallback
-    else
-      return (mod and mod[key] and mod[key][type]) or fallback
-    end
-  end
-end
 
-local function get_option_for(key)
-  return function(lang, type)
-    local mod = OPTIONS.languages[lang]
-    return mod and mod.options and mod.options[key] and mod.options[key][type]
-  end
-end
 
+
+-- INIT
 function M.init(overrides)
   for name, mod in pairs(overrides.languages) do
     if mod.extends and overrides.languages[mod.extends] then
@@ -41,18 +27,51 @@ function M.setup(opts)
   end
 end
 
-function M.identity(x)
-  return x
+
+
+
+
+
+
+--- OPERATION HELPERS
+-- TODO: Remove these in favour of node helpers and default handlers
+function M.trim_end(op, node, bufnr, winnr, row, col)
+  local first_line = M.get_line(bufnr, row)
+
+  local trimmed = first_line:gsub('%s+$', '')
+  vim.api.nvim_buf_set_lines(bufnr,
+                             row,
+                             row + 1,
+                             true,
+                             { trimmed })
+  if op == 'join' then
+    vim.cmd.norm(row..'GJ')
+    M.jump_to_node_end_at(op, node, bufnr, winnr, row, col + 1, 0, trimmed:len())
+  else
+    M.jump_to_node_end_at(op, node, bufnr, winnr, row + 1, col, 1, -1)
+  end
 end
 
-function M.split(str, sep, opts)
-  opts = vim.tbl_extend('keep', opts or {}, { plain = true, trimempty = true })
-  return vim.split(str, sep, opts)
+function M.jump_to_node_end_at(op, orig_node, bufnr, winnr, row, col, row_offset, col_offset)
+  local found, new_node = pcall(get_node_at_pos, bufnr,
+                                             row,
+                                             col,
+                                             { ignore_injections = false })
+  if found and new_node then
+    local _, _, end_row, end_col = new_node:range()
+
+    vim.api.nvim_win_set_cursor(winnr, {
+      end_row + (row_offset or 1),
+      end_col + (col_offset or -1),
+    })
+  end
 end
 
-function M.dedupe(sep)
-  return function(line)
-    return not line:find('^%s*'..sep..'%s*$')
+function M.get_joined(lang, type, sep, joined)
+  if M.node_is_sep_first(lang, type) then
+    return joined:gsub('%s*%'..sep, sep)
+  else
+    return joined:gsub(sep..'%s*', sep..' ')
   end
 end
 
@@ -78,40 +97,12 @@ function M.get_line(bufnr, row)
   return line
 end
 
-function M.get_base_indent(node)
-  local row = node:range()
-  return M.get_line(0, row):match'^%s+' or ''
-end
-
-function M.get_joined(lang, type, sep, joined)
-  if M.node_is_sep_first(lang, type) then
-    return joined:gsub('%s*%'..sep, sep)
-  else
-    return joined:gsub(sep..'%s*', sep..' ')
-  end
-end
 
 
---- trim first line
-function M.trim_end(op, node, bufnr, winnr, row, col)
-  local first_line = M.get_line(bufnr, row)
 
-  local trimmed = first_line:gsub('%s+$', '')
-  vim.api.nvim_buf_set_lines(bufnr,
-                             row,
-                             row + 1,
-                             true,
-                             { trimmed })
-  if op == 'join' then
-    vim.cmd.norm(row..'GJ')
-    M.jump_to_node_end_at(op, node, bufnr, winnr, row, col + 1, 0, trimmed:len())
-  else
-    M.jump_to_node_end_at(op, node, bufnr, winnr, row + 1, col, 1, -1)
-  end
-end
 
-local get_node_at_pos = vim.treesitter.get_node_at_pos
 
+-- NODE HELPERS
 function M.cursor_to_node_end(original_node)
   local row, col = original_node:range()
   local found, node = pcall(vim.treesitter.get_node_at_pos, 0, row, col, { ignore_injections = false })
@@ -121,21 +112,67 @@ function M.cursor_to_node_end(original_node)
   end
 end
 
-function M.jump_to_node_end_at(op, orig_node, bufnr, winnr, row, col, row_offset, col_offset)
-  local found, new_node = pcall(get_node_at_pos, bufnr,
-                                             row,
-                                             col,
-                                             { ignore_injections = false })
-  if found and new_node then
-    local _, _, end_row, end_col = new_node:range()
-
-    vim.api.nvim_win_set_cursor(winnr, {
-      end_row + (row_offset or 1),
-      end_col + (col_offset or -1),
-    })
-  end
+function M.is_child_of(type, node)
+  local current = node
+  repeat
+    if current:type() == type then
+      return true
+    end
+    current = current:parent()
+  until not current:parent()
+  return false
 end
 
+function M.replace_node(node, replacement)
+  local row, col, row_end, col_end = node:range()
+  local base_indent = M.get_base_indent(node) or ''
+  local starts_newline = replacement:match'^\n'
+  local lines = M.split(replacement, '\n')
+        print(vim.inspect(lines))
+  for i, line in ipairs(lines) do
+    if i > 1 then
+      lines[i] = base_indent..line
+    end
+  end
+  if starts_newline then table.insert(lines, 1, '') end
+  vim.api.nvim_buf_set_text(0,
+                            row,
+                            col,
+                            row_end,
+                            col_end,
+                            lines)
+end
+
+function M.get_base_indent(node)
+  local row = node:range()
+  return M.get_line(0, row):match'^%s+' or ''
+end
+
+function M.trim_node_line_end(node)
+  local row = node:range()
+  local trimmed = M.get_line(0, row):gsub('%s*$', '')
+  vim.api.nvim_buf_set_lines(0,
+                             row,
+                             row + 1,
+                             true,
+                             { trimmed })
+end
+
+function M.join_with_previous_line(node)
+  local row = node:range()
+  local previous = M.get_line(0, row - 1)
+  local current = M.get_line(0, row)
+  vim.api.nvim_buf_set_lines(0,
+                             row - 1,
+                             row,
+                             true,
+                             { previous .. ' ' .. current })
+end
+
+
+
+
+-- STRING HELPERS
 function M.is_lengthy(str)
   return str:len() > 0
 end
@@ -146,21 +183,38 @@ function M.normalize_item(sep)
   end
 end
 
-function M.replace_node(node, replacement)
-  local row, col, row_end, col_end = node:range()
-  local base_indent = M.get_base_indent(node) or ''
-  local lines = M.split(replacement, '\n')
-  for i, line in ipairs(lines) do
-    if i > 1 then
-      lines[i] = base_indent..line
+function M.split(str, sep, opts)
+  opts = vim.tbl_extend('keep', opts or {}, { plain = true, trimempty = true })
+  return vim.split(str, sep, opts)
+end
+
+function M.dedupe(sep)
+  return function(line)
+    return not line:find('^%s*'..sep..'%s*$')
+  end
+end
+
+
+
+
+-- CONFIG AND OPTS
+local function get_config_for(key, fallback)
+  return function(lang, type, op)
+    local mod = DEFAULTS.languages[lang]
+    -- TODO: make everything a node handler
+    if key == 'handlers' then
+      return (mod and mod[key] and mod[key][type] and mod[key][type][op]) or fallback
+    else
+      return (mod and mod[key] and mod[key][type]) or fallback
     end
   end
-  vim.api.nvim_buf_set_text(0,
-                            row,
-                            col,
-                            row_end,
-                            col_end,
-                            lines)
+end
+
+local function get_option_for(key)
+  return function(lang, type)
+    local mod = OPTIONS.languages[lang]
+    return mod and mod.options and mod.options[key] and mod.options[key][type]
+  end
 end
 
 M.node_is_sep_first = get_option_for('sep_first')
