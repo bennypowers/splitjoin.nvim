71f3dede876d18567d5d847cb4b5a6233639734d
Benny Powers <web@bennypowers.com>
2023-02-27 10:19:10 +0200

    style: whitespace

--- lua/splitjoin/languages/lua/functions.lua
+++ lua/splitjoin/languages/lua/functions.lua
@@ -1,6 +1,7 @@
 local Node = require'splitjoin.util.node'
 local String = require'splitjoin.util.string'
 
+local get_text = Node.get_text
 local Lua = {}
 
 function Lua.split_function(node, options)
@@ -8,22 +9,16 @@ function Lua.split_function(node, options)
   local append, get = String.append('')
   for child in node:iter_children() do
     local type = child:type()
-    if type == 'local' then
-      append(Node.get_text(child), ' ')
+    if type == 'local' then append(get_text(child), ' ')
+    elseif type == 'parameters' then append(get_text(child), '\n')
+    elseif type == 'block' then append(indent, get_text(child))
+    elseif type == 'end' then append('\n', 'end')
     elseif type == 'function' then
       append('function')
       if node:type() == 'function_declaration' then
         append(' ')
       end
-    elseif type == 'parameters' then
-      append(Node.get_text(child), '\n')
-    elseif type == 'block' then
-      append(indent, Node.get_text(child))
-    elseif type == 'end' then
-      append('\n', 'end')
-    else
-      local line = Node.get_text(child)
-      append(line)
+    else append(get_text(child))
     end
   end
   Node.replace(node, get())
@@ -34,17 +29,13 @@ function Lua.join_function(node, options)
   local append, get = String.append('')
   for child in node:iter_children() do
     local type = child:type()
-    if type == 'local' or type == 'parameters' then
-      append(Node.get_text(child), ' ')
+    if type == 'local' or type == 'parameters' then append(get_text(child), ' ')
     elseif type == 'function' then
       append('function')
       if node:type() == 'function_declaration' then append(' ') end
-    elseif type == 'block' then
-      append(Node.get_text(child), ' ')
-    elseif type == 'end' then
-      append('end')
-    else
-      append(Node.get_text(child))
+    elseif type == 'block' then append(get_text(child), ' ')
+    elseif type == 'end' then append('end')
+    else append(get_text(child))
     end
   end
   Node.replace(node, get())
@@ -56,21 +47,15 @@ function Lua.split_if(node, options)
   local append, get = String.append('')
   for child in node:iter_children() do
     local type = child:type()
-    if type == 'if' then
-      append('if ', Node.get_text(child:next_sibling()), ' then\n')
-    elseif type == 'end' then
-      append('\nend')
-    elseif type == 'block' then
-      append(indent, vim.trim(Node.get_text(child)))
+    if type == 'if' then append('if ', get_text(child:next_sibling()), ' then\n')
+    elseif type == 'end' then append('\nend')
+    elseif type == 'block' then append(indent, vim.trim(get_text(child)))
     elseif type == 'elseif_statement' or type == 'else_statement' then
-      for grandchild in child:iter_children() do
-        local gctype = grandchild:type()
-        if gctype == 'elseif' then
-          append('\nelseif ', Node.get_text(grandchild:next_sibling()), ' then\n')
-        elseif gctype == 'else' then
-          append('\nelse\n')
-        elseif gctype == 'block' then
-          append(indent, vim.trim(Node.get_text(grandchild)))
+      for gc in child:iter_children() do
+        local  gctype = gc:type()
+        if     gctype == 'elseif' then append('\nelseif ', get_text(gc:next_sibling()), ' then\n')
+        elseif gctype == 'else'   then append('\nelse\n')
+        elseif gctype == 'block'  then append(indent, vim.trim(get_text(gc)))
         end
       end
     end
@@ -80,30 +65,23 @@ function Lua.split_if(node, options)
 end
 
 function Lua.join_if(node)
-  local replacement = ''
-  local function append(s, t) replacement = replacement .. s .. (t or '') end
+  local append, get = String.append('')
   for child in node:iter_children() do
     local type = child:type()
-    if type == 'if' then
-      append('if '..Node.get_text(child:next_sibling())..' then ')
-    elseif type == 'block' then
-      append(Node.get_text(child), ' ')
+    if type == 'if' then append('if ', get_text(child:next_sibling()), ' then ')
+    elseif type == 'end' then append('end')
+    elseif type == 'block' then append(get_text(child), ' ')
     elseif type == 'elseif_statement' or type == 'else_statement' then
-      for grandchild in child:iter_children() do
-        local gctype = grandchild:type()
-        if gctype == 'elseif' then
-          append('elseif '..Node.get_text(grandchild:next_sibling())..' then', ' ')
-        elseif gctype == 'else' then
-          append('else', ' ')
-        elseif gctype == 'block' then
-          append(vim.trim(Node.get_text(grandchild)), ' ')
+      for gc in child:iter_children() do
+        local  gctype = gc:type()
+        if     gctype == 'elseif' then append('elseif ', get_text(gc:next_sibling()), ' then ')
+        elseif gctype == 'else'   then append('else', ' ')
+        elseif gctype == 'block'  then append(vim.trim(get_text(gc)), ' ')
         end
       end
-    elseif type == 'end' then
-      append('end')
     end
   end
-  Node.replace(node, replacement)
+  Node.replace(node, get())
   Node.goto_node(node)
 end
 
