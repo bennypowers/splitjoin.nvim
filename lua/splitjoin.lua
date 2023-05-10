40adbcfbd99cd3d08d15a688191946546d95cedd
Benny Powers <web@bennypowers.com>
2023-05-10 15:17:44 +0300

    fix: remove log

--- lua/splitjoin.lua
+++ lua/splitjoin.lua
@@ -26,7 +26,6 @@ local function get_operable_node_under_cursor(bufnr, winnr)
         tsparser:parse()
   local langtree = tsparser:language_for_range(cursor_range);
   local tstree = langtree:tree_for_range(cursor_range, { ignore_injections = false }) or langtree:trees()[1]
-  vim.notify(langtree:lang())
   if not tstree then return nil, nil end
   local lang = langtree:lang()
   local query = get_query(lang, 'splitjoin')
