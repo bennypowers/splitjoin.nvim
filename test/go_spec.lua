5c00f0b5877ad25897f13aac901804fff189bf64
Benny Powers <web@bennypowers.com>
2025-07-08 23:03:24 +0300

    test: refactor tests

--- test/go_spec.lua
+++ test/go_spec.lua
@@ -1,113 +1,102 @@
-local helpers = require'test.helpers'
 local d = require'plenary.strings'.dedent
+local H = require'test.helpers'
 
-describe("go", function()
-  describe("struct", function()
-    local joined = [[
-type User struct { Name string; Age int; Email string }
-]]
+local lang = 'go'
 
-    local split = [[
-type User struct {
-  Name string
-  Age int
-  Email string
-}
-]]
+describe(lang, function()
+  H.make_suite(lang, 'struct',
+    d[[
+      type User struct { Name string; Age int; Email string }
+    ]],
+    d[[
+      type User struct {
+        Name string
+        Age int
+        Email string
+      }
+    ]],
+    '{'
+  )
 
-    helpers.make_suite('go', 'splits and joins struct fields', joined, split, '{')
-  end)
+  H.make_suite(lang, 'params',
+    d[[
+      func Foo(a int, b string) (int, error) {}
+    ]],
+    d[[
+      func Foo(
+        a int,
+        b string,
+      ) (int, error) {}
+    ]],
+    'a'
+  )
 
-  describe("params", function()
-    local joined = [[
-func Foo(a int, b string) (int, error) {}
-]]
+  H.make_suite(lang, 'return',
+    d[[
+      func Foo(a int, b string) (int, error) {}
+    ]],
+    d[[
+      func Foo(a int, b string) (
+        int,
+        error,
+      ) {}
+    ]],
+    'error'
+  )
 
-    local split = [[
-func Foo(
-  a int,
-  b string,
-) (int, error) {}
-]]
+  H.make_suite(lang, 'args',
+    d[[
+      Foo(a, b, c)
+    ]],
+    d[[
+      Foo(
+        a,
+        b,
+        c,
+      )
+    ]],
+    'a'
+  )
 
-    helpers.make_suite('go', 'splits and joins function params', joined, split, 'a')
-  end)
-
-  describe("return", function()
-    local joined = [[
-func Foo(a int, b string) (int, error) {}
-]]
-
-    local split = [[
-func Foo(a int, b string) (
-  int,
-  error,
-) {}
-]]
-
-    helpers.make_suite('go', 'splits and joins return types', joined, split, 'error')
-  end)
-  describe("args", function()
-    local joined = [[
-Foo(a, b, c)
-]]
-
-    local split = [[
-Foo(
-  a,
-  b,
-  c,
-)
-]]
-
-    helpers.make_suite('go', 'splits and joins function arguments', joined, split, 'a')
-  end)
-
-  describe("slice", function()
-    local joined = d[=[
+  H.make_suite(lang, 'slice',
+    d[=[
       []string{"a", "b", "c"}
-    ]=]
-
-    local split = d[=[
+    ]=],
+    d[=[
       []string{
         "a",
         "b",
         "c",
       }
-    ]=]
+    ]=],
+    '{'
+  )
 
-    helpers.make_suite('go', 'splits and joins slice literals', joined, split, '{')
-  end)
-
-  describe("map", function()
-    local joined = d[=[
+  H.make_suite(lang, 'map',
+    d[=[
       map[string]int{"a": 1, "b": 2, "c": 3}
-    ]=]
-
-    local split = d[=[
+    ]=],
+    d[=[
       map[string]int{
         "a": 1,
         "b": 2,
         "c": 3,
       }
-    ]=]
+    ]=],
+    '{'
+  )
 
-    helpers.make_suite('go', 'splits and joins map literals', joined, split, '{')
-  end)
-
-  describe("composite literal", function()
-    local joined = d[=[
+  H.make_suite(lang, 'composite literal',
+    d[=[
       User{Name: "a", Age: 1, Email: "b"}
-    ]=]
-
-    local split = d[=[
+    ]=],
+    d[=[
       User{
         Name: "a",
         Age: 1,
         Email: "b",
       }
-    ]=]
-
-    helpers.make_suite('go', 'splits and joins composite literals', joined, split, '{')
-  end)
+    ]=],
+    '{'
+  )
 end)
